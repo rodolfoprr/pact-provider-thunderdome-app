cbc8ca800b9519c20f9b0a27067efed39d9c2e91
Rodolfo Pereira <rodolfo@pragma.team>
2018-03-05 15:28:47 +1100

    Get Pact file from consumer project

--- thunderdome/thunderdome_test.go
+++ thunderdome/thunderdome_test.go
@@ -1,18 +1,12 @@
 package thunderdome_test
 
 import (
-	"fmt"
-	"os"
 	"pact-provider-thunderdome-app/thunderdome"
-	"path/filepath"
 	"testing"
 
 	"github.com/pact-foundation/pact-go/dsl"
 	"github.com/pact-foundation/pact-go/types"
 )
-
-var dir, _ = os.Getwd()
-var pactDir = fmt.Sprintf("%s/../pacts", dir)
 
 func TestProvider(t *testing.T) {
 	go thunderdome.Start()
@@ -25,6 +19,6 @@ func TestProvider(t *testing.T) {
 
 	pact.VerifyProvider(t, types.VerifyRequest{
 		ProviderBaseURL: "http://localhost:8000",
-		PactURLs:        []string{filepath.ToSlash(fmt.Sprintf("%s/SpaceX-Thunderdome.json", pactDir))},
+		PactURLs:        []string{"https://raw.githubusercontent.com/rodolfoprr/pact-consumer-spacex-app/master/target/pacts/SpaceX-Thunderdome.json"},
 	})
 }
